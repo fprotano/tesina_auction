fd3660a7c601e90a154bfc22e36fe9b1388033f5
david <david@DESKTOP-597K9R6.fritz.box>
2021-05-28 11:26:03 +0200

    auctionOrderTransactionLog complete lap //by Simone

--- TesinaAuction/src/it/exolab/tesina/auction/service/api/AuctionOrderTransactionLogService.java
+++ TesinaAuction/src/it/exolab/tesina/auction/service/api/AuctionOrderTransactionLogService.java
@@ -1,8 +1,36 @@
 package it.exolab.tesina.auction.service.api;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import it.exolab.tesina.auction.api.model.dto.AuctionOrderTransactionLogDTO;
+import it.exolab.tesina.auction.repository.AuctionOrderTransactionLogRepo;
+
 public class AuctionOrderTransactionLogService {
-	
-	
-	
+
+	private AuctionOrderTransactionLogRepo auctionOrderTransactionLogRepo;
+
+	@Autowired(required = true)
+	public void setAuctionOrderTransactionLogRepo(AuctionOrderTransactionLogRepo auctionOrderTransactionLogRepo) {
+		this.auctionOrderTransactionLogRepo = auctionOrderTransactionLogRepo;
+
+	}
+
+	public AuctionOrderTransactionLogDTO find(int id) {
+		return this.auctionOrderTransactionLogRepo.findOne(id);
+	}
+
+	public List<AuctionOrderTransactionLogDTO> findAll() {
+		return (List<AuctionOrderTransactionLogDTO>) auctionOrderTransactionLogRepo.findAll();
+	}
+
+	public void save(AuctionOrderTransactionLogDTO model) {
+		this.auctionOrderTransactionLogRepo.save(model);
+	}
+
+	public void delete(int id) {
+		auctionOrderTransactionLogRepo.delete(id);
+	}
 
 }

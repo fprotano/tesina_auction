4acda5bd5a24c741e067624b488cb73e4b731b46
david <david@DESKTOP-597K9R6.fritz.box>
2021-05-28 14:08:07 +0200

    by

--- TesinaAuction/src/it/exolab/tesina/auction/repository/AutionBidRepo.java
+++ TesinaAuction/src/it/exolab/tesina/auction/repository/AutionBidRepo.java
@@ -5,6 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import it.exolab.tesina.auction.api.model.dto.AuctionBidDTO;
 
 
-public interface AutionBidRepo extends CrudRepository<AuctionBidDTO, Integer>{
+public interface AutionBidRepo extends  CrudRepository<AuctionBidDTO, Integer>{
 
 }
+

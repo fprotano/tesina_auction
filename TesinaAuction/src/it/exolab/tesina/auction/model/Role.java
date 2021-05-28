22f43cacd5b0c16ee57c6370c4746f3054350f33
david <david@DESKTOP-597K9R6.fritz.box>
2021-05-28 11:42:02 +0200

    //by simone

--- TesinaAuction/src/it/exolab/tesina/auction/model/Role.java
+++ TesinaAuction/src/it/exolab/tesina/auction/model/Role.java
@@ -1,6 +1,16 @@
 package it.exolab.tesina.auction.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Role {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String title;
 

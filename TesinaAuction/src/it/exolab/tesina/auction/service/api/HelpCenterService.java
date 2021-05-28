cb47ee479f833c790b20ca164ca548bff9d2f9bf
david <david@DESKTOP-597K9R6.fritz.box>
2021-05-28 12:27:53 +0200

    helpCenterService lap complete //by simone

--- TesinaAuction/src/it/exolab/tesina/auction/service/api/HelpCenterService.java
+++ TesinaAuction/src/it/exolab/tesina/auction/service/api/HelpCenterService.java
@@ -1,5 +1,34 @@
 package it.exolab.tesina.auction.service.api;
 
-public class HelpCenterService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import it.exolab.tesina.auction.api.model.dto.HelpCenterDTO;
+import it.exolab.tesina.auction.repository.HelpCenterRepo;
+
+public class HelpCenterService {
+	private HelpCenterRepo helpCenterRepo;
+
+	@Autowired(required = true)
+	public void setAuctionRepo(HelpCenterRepo helpCenterRepo) {
+		this.helpCenterRepo = helpCenterRepo;
+
+	}
+
+	public HelpCenterDTO find(int id) {
+		return helpCenterRepo.findOne(id);
+	}
+
+	public List<HelpCenterDTO> findAll() {
+		return (List<HelpCenterDTO>) helpCenterRepo.findAll();
+	}
+
+	public void save(HelpCenterDTO model) {
+		this.helpCenterRepo.save(model);
+	}
+
+	public void delete(int id) {
+		helpCenterRepo.delete(id);
+	}
 }

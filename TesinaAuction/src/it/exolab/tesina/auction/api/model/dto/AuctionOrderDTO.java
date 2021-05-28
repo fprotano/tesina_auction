438513017c382d2b84e6040f61d736756e73bd59
alessio <alessio@DESKTOP-P6TQ6T6.localdomain>
2021-05-28 10:39:00 +0200

    zdfdfgd

--- TesinaAuction/src/it/exolab/tesina/auction/api/model/dto/AuctionOrderDTO.java
+++ TesinaAuction/src/it/exolab/tesina/auction/api/model/dto/AuctionOrderDTO.java
@@ -2,6 +2,6 @@ package it.exolab.tesina.auction.api.model.dto;
 
 import it.exolab.tesina.auction.model.AuctionOrder;
 
-public class AuctionOrderDTO extends AuctionOrder{
+public class AuctionOrderDTO extends AuctionOrder {
 
 }

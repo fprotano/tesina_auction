28aaa320a505bfbcdb6db221feea68600ce847ba
alessio <alessio@DESKTOP-P6TQ6T6.localdomain>
2021-05-29 18:39:44 +0200

    query con where

--- TesinaAuction/src/it/exolab/tesina/auction/repository/AuctionRepo.java
+++ TesinaAuction/src/it/exolab/tesina/auction/repository/AuctionRepo.java
@@ -12,12 +12,24 @@ import it.exolab.tesina.auction.model.Auction;
 
 public interface AuctionRepo extends CrudRepository<Auction, Integer>{
 	
+//	@Transactional
+//	@Modifying
+//	@Query("SELECT a FROM it.exolab.tesina.auction.model.Auction AS a "
+//			+ "INNER JOIN it.exolab.tesina.auction.model.UserItem AS ui ON a.userItemId = ui.id "
+//			+ "INNER JOIN it.exolab.tesina.auction.model.User AS u ON ui.userId = u.id "
+//			+ "WHERE u.id=?1")
+//	public List<Auction> findAuctionsbyUser(Integer idUser);
+	
 	@Transactional
 	@Modifying
-	@Query("SELECT a FROM it.exolab.tesina.auction.model.Auction AS a "
-			+ "INNER JOIN it.exolab.tesina.auction.model.UserItem AS ui ON a.userItemId = ui.id "
-			+ "INNER JOIN it.exolab.tesina.auction.model.User AS u ON ui.userId = u.id "
-			+ "WHERE u.id=?1")
+	@Query("SELECT a FROM it.exolab.tesina.auction.model.Auction AS a, "
+			+ "it.exolab.tesina.auction.model.UserItem AS ui, "
+			+ "it.exolab.tesina.auction.model.User AS u "
+			+ "WHERE a.userItemId = ui.id AND "
+			+ "ui.userId = u.id AND "
+			+ "u.id=?1")
 	public List<Auction> findAuctionsbyUser(Integer idUser);
+	
+	
 
 }

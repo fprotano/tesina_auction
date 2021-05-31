0e03101b91b0a5a64d2d892930e9312e21bb142c
david <david@DESKTOP-597K9R6.fritz.box>
2021-05-31 21:15:19 +0200

    time for angular conversion method // by simone *_*

--- TesinaAuction/src/it/exolab/tesina/auction/controller/BaseController.java
+++ TesinaAuction/src/it/exolab/tesina/auction/controller/BaseController.java
@@ -2,6 +2,10 @@ package it.exolab.tesina.auction.controller;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -47,4 +51,38 @@ public class BaseController<T> {
 			}
 		return ret;	
 	}
+	
+	public Object conversionTimeRetobj(Object a) { // metodo conversione data-format return obj
+		Field[] fields = a.getClass().getDeclaredFields();
+		Object convert = null;
+		for (int i = 0; i < fields.length; i++) {
+			System.out.println(fields[i].getType().getTypeName().contains("Timestamp")); //
+			if (fields[i].getType().getTypeName().contains("Timestamp")) {
+
+				try {
+					Date date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(fields[i].get(a).toString());
+					System.out.println(date);
+					fields[i].set(date, "DateForClient"+i);
+					convert = fields;
+					
+
+				} catch (IllegalArgumentException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+
+		}
+	  return convert;
+
+	}
 }
+	
+

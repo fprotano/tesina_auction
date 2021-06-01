44d61ec09a02ae97541feb2436eae35c67f1e355
andre <andre@192.168.1.121>
2021-06-01 14:14:52 +0200

    modificato AuctionBidRepo (e modelli connessi)

--- TesinaAuction/src/it/exolab/tesina/auction/controller/api/ApiAuctionBidController.java
+++ TesinaAuction/src/it/exolab/tesina/auction/controller/api/ApiAuctionBidController.java
@@ -23,58 +23,58 @@ public class ApiAuctionBidController extends BaseController{
 	
 	// insert, findbyid, findByAuctionID, findByUserIdWhereClosedAt=null
 	
-//	private AuctionBidService auctionBidSer;
-//	
-//	@Autowired(required = false)
-//	public void setAuctionBidService(AuctionBidService abS) {
-//		this.auctionBidSer = abS;
-//	}
-//	
-//	
-//	@RequestMapping(value = "insertBid", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-//	@ResponseBody
-//	public HttpResponse<AuctionBid> insertBid(@RequestBody AuctionBid model) {
-//		
-//		this.auctionBidSer.save(model);
-//		return (HttpResponse<AuctionBid>) sendSuccess(model);
-//		
-//	}
-//	
-//	@RequestMapping(value = "findAllBids", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-//	@ResponseBody
-//	public HttpResponse<AuctionBid> findAllBids(@RequestBody AuctionBid model) {
-//		
-//		List <AuctionBid> listBids = this.auctionBidSer.findAll();
-//		return (HttpResponse<AuctionBid>) sendSuccess(listBids);
-//		
-//	}
-//	
-//	@RequestMapping(value = "auctionBids", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-//	@ResponseBody
-//	public HttpResponse<AuctionBid> findBidsByAuctionId(@RequestBody String auctionId) {
-//		
-//		List <AuctionBid> listBids = this.auctionBidSer.findBidsByAuctionId(Integer.parseInt(auctionId));
-//		return (HttpResponse<AuctionBid>) sendSuccess(listBids);
-//		
-//	}
-//	
-//	@RequestMapping(value = "userActiveAuctionBids", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-//	@ResponseBody
-//	public HttpResponse<AuctionBid> findBidsOfActiveAuctionsByUserId(@RequestBody String userId) {
-//		
-//		List <AuctionBid> listBids = this.auctionBidSer.findBidsOfActiveAuctionsByUserId(Integer.parseInt(userId));
-//		return (HttpResponse<AuctionBid>) sendSuccess(listBids);
-//		
-//	}
-//	
-//	@RequestMapping(value = "userLastActiveAuctionBids", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-//	@ResponseBody
-//	public HttpResponse<AuctionBid> findLastBidsOfActiveAuctionsByUserId(@RequestBody String userId) {
-//		
-//		List <AuctionBid> listBids = this.auctionBidSer.findLastBidsOfActiveAuctionsByUserId(Integer.parseInt(userId));
-//		return (HttpResponse<AuctionBid>) sendSuccess(listBids);
-//		
-//	}
+	private AuctionBidService auctionBidSer;
+	
+	@Autowired(required = false)
+	public void setAuctionBidService(AuctionBidService abS) {
+		this.auctionBidSer = abS;
+	}
+	
+	
+	@RequestMapping(value = "insertBid", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public HttpResponse<AuctionBid> insertBid(@RequestBody AuctionBid model) {
+		
+		this.auctionBidSer.save(model);
+		return (HttpResponse<AuctionBid>) sendSuccess(model);
+		
+	}
+	
+	@RequestMapping(value = "findAllBids", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public HttpResponse<AuctionBid> findAllBids(@RequestBody AuctionBid model) {
+		
+		List <AuctionBid> listBids = this.auctionBidSer.findAll();
+		return (HttpResponse<AuctionBid>) sendSuccess(listBids);
+		
+	}
+	
+	@RequestMapping(value = "auctionBids", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public HttpResponse<AuctionBid> findBidsByAuctionId(@RequestBody String auctionId) {
+		
+		List <AuctionBid> listBids = this.auctionBidSer.findBidsByAuctionId(Integer.parseInt(auctionId));
+		return (HttpResponse<AuctionBid>) sendSuccess(listBids);
+		
+	}
+	
+	@RequestMapping(value = "userActiveAuctionBids", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public HttpResponse<AuctionBid> findBidsOfActiveAuctionsByUserId(@RequestBody String userId) {
+		
+		List <AuctionBid> listBids = this.auctionBidSer.findBidsOfActiveAuctionsByUserId(Integer.parseInt(userId));
+		return (HttpResponse<AuctionBid>) sendSuccess(listBids);
+		
+	}
+	
+	@RequestMapping(value = "userLastActiveAuctionBids", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public HttpResponse<AuctionBid> findLastBidsOfActiveAuctionsByUserId(@RequestBody String userId) {
+		
+		List <AuctionBid> listBids = this.auctionBidSer.findLastBidsOfActiveAuctionsByUserId(Integer.parseInt(userId));
+		return (HttpResponse<AuctionBid>) sendSuccess(listBids);
+		
+	}
 	
 	
 

21e59bb0719cbfe9326096a8e0be23fd8af7591b
david <david@DESKTOP-597K9R6.fritz.box>
2021-06-03 10:33:03 +0200

    by simo

--- TesinaAuction/src/it/exolab/tesina/auction/controller/StaffController.java
+++ TesinaAuction/src/it/exolab/tesina/auction/controller/StaffController.java
@@ -1,4 +1,4 @@
-package it.exolab.tesina.auction.controller.api;
+package it.exolab.tesina.auction.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import it.exolab.tesina.auction.api.model.HttpResponse;
 import it.exolab.tesina.auction.controller.BaseController;
@@ -16,8 +17,8 @@ import it.exolab.tesina.auction.service.api.StaffService;
 
 @CrossOrigin
 @Controller
-@RequestMapping(value = "api/staff")
-public class ApiControllerStaff extends BaseController<Staff> {
+@RequestMapping(value = "staff")
+public class StaffController extends BaseController<Staff> {
 	private StaffService staffService;
 
 	@Autowired(required = true)
@@ -26,11 +27,13 @@ public class ApiControllerStaff extends BaseController<Staff> {
 	}
 
 	@RequestMapping(value = "staffInsertUpdate", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
-	public HttpResponse<Staff> doStaffInsertUpdate(@RequestBody Staff model) {
-		System.out.println("nel StaffInsert, auction > " + model);
+
+	public  ModelAndView  doStaffInsertUpdate(Staff model) {
+		//System.out.println("nel StaffInsert, auction > " + model);
 		staffService.save(model);
-		return sendSuccess(model);
+		
+		return null;
+	    
 	}
 
 	@RequestMapping(value = "findById", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)

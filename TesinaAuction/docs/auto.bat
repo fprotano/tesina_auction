602474fa11ee0920a9eb21aa65abfd7aef657c13
david <david@DESKTOP-597K9R6.station>
2021-06-04 09:18:46 +0200

    modificare bat con aprire con editor e modificare con directory es cd:ecc  //by simone

---
+++ TesinaAuction/docs/auto.bat
@@ -0,0 +1,9 @@
+@echo off
+cls
+echo Programma Cancellazione Aste_auto
+cd C:\sviluppo\workspace\auto_update\src\auto_update
+javac AutoUpdate.java
+java AutoUpdate
+
+
+pause
---
+++ TesinaAuction/src/it/exolab/tesina/exploid/bat/forauction/AutoUpdate.java
@@ -0,0 +1,44 @@
+package it.exolab.tesina.exploid.bat.forauction;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class AutoUpdate {
+public static void main(String[]args) {
+	 Connection conn = null;
+     Statement stmt = null;
+     try {
+        try {
+           Class.forName("com.mysql.jdbc.Driver");
+        } catch (Exception e) {
+           System.out.println(e);
+        }
+        conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/tesina_auction", "root", "root");
+        System.out.println("Connessione effetuata:");
+        stmt = (Statement) conn.createStatement();
+        String query1 = "update auction SET  closed_auction_at = now() where NOW() > end_auction_at; ";
+        stmt.executeUpdate(query1);
+        System.out.println("Record aggiornato .........-------->");
+     } catch (SQLException e) {
+      
+     } catch (Exception x) {
+        
+     } finally {
+        try {
+           if (stmt != null)
+           conn.close();
+        } catch (SQLException se) {}
+        try {
+           if (conn != null)
+           conn.close();
+        } catch (SQLException se) {
+           se.printStackTrace();
+        }
+     }
+    
+  }
+
+}
+

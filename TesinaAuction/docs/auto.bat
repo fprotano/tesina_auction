31f7872db72706bcb8e377044979e7ed95c01e73
david <david@DESKTOP-597K9R6.fritz.box>
2021-06-08 15:07:17 +0200

    by simo

--- TesinaAuction/docs/auto.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cls
-echo Programma Cancellazione Aste_auto
-cd C:\dev\git\tesina_auction\TesinaAuction\src\it\exolab\tesina\exploid\bat\forauction\
-javac AutoUpdate.java
-java AutoUpdate.java
-
-
-pause

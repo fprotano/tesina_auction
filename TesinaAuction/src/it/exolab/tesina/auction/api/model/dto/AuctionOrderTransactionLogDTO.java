55fa61fbb79267c45c95b7177c1188bf7d25922a
andre <andre@DESKTOP-J3IIPOV.fritz.box>
2021-07-01 15:43:32 +0200

    tolto classe aggiunta per sbaglio

--- TesinaAuction/src/it/exolab/tesina/auction/api/model/dto/AuctionOrderTransactionLogDTO.java
+++
@@ -1,7 +0,0 @@
-package it.exolab.tesina.auction.api.model.dto;
-
-import it.exolab.tesina.auction.model.AuctionOrderTransactionLog;
-
-public class AuctionOrderTransactionLogDTO extends AuctionOrderTransactionLog {
-
-}
